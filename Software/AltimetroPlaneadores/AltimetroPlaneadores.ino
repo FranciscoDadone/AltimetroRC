f7d530fb7ad2c9ff03c593bf6ab7a1258295800c
Francisco Dadone <dadonefran@gmail.com>
2023-07-22 03:52:30 -0300

    Tira los metros subidos

--- Software/AltimetroPlaneadores/AltimetroPlaneadores.ino
+++ Software/AltimetroPlaneadores/AltimetroPlaneadores.ino
@@ -1,10 +1,10 @@
 #include <Wire.h>
 #include <Servo.h>
 #include "SparkFunBME280.h"
-#include <TimerOne.h>
+#include <MsTimer2.h>
 
-#define TIEMPO_MAX 30
-#define ALTURA_MAX 100
+#define TIEMPO_MAX 100
+#define ALTURA_MAX 2
 
 #define PIN_IN 3
 #define PIN_OUT 2
@@ -28,8 +28,10 @@ struct {
 } motor;
 
 struct {
-  int tiempo;
+  int tiempo = TIEMPO_MAX * 10;
   int sensor;
+  int led;
+  int led_d;
 } tiempos;
 
 int estado = ESPERA;
@@ -38,8 +40,9 @@ void setup() {
   pinMode(PIN_IN, INPUT_PULLUP);
   pinMode(13, OUTPUT);
 
-  Timer1.initialize(1000);
-  Timer1.attachInterrupt(interrupt_1ms);
+  MsTimer2::set(100, interrupt);
+  MsTimer2::start();
+
   Wire.begin();
   Serial.begin(9600);
 
@@ -49,23 +52,32 @@ void setup() {
   if(bmp280.beginI2C() == false) Serial.println("Fallo BMP280");
   
   ESC.writeMicroseconds(1000);
-  delay(5000);
 }
 
-void interrupt_1ms (void) {
+void interrupt() {
   if (tiempos.tiempo && estado == INICIADO) tiempos.tiempo--;
   if (tiempos.sensor) tiempos.sensor--;
+  if (tiempos.led) tiempos.led--;
 }
 
 void leer_sensor (void);
+void actualizar_led_altura (void);
 
 void loop() {
   motor.vel = pulseIn(PIN_IN, HIGH);
 
   switch (estado) {
+    case PARADO:
+      ESC.writeMicroseconds(1000);
+      leer_sensor();
+      if (!tiempos.led) {
+        if (tiempos.led_d) tiempos.led_d--;
+        actualizar_led_altura(altura.maxima);
+        tiempos.led = 5;
+      }
+      break;
     case ESPERA:
       if (motor.vel >= 1300) {
-        tiempos.tiempo = TIEMPO_MAX * 1000;
         leer_sensor();
         altura.inicial = altura.actual;
         estado = INICIADO;
@@ -75,16 +87,14 @@ void loop() {
     case INICIADO:
       leer_sensor();
       ESC.writeMicroseconds(motor.vel);
+
+      int diff = altura.actual - altura.inicial;
       
-      if (motor.vel < 1300 || !tiempos.tiempo || (altura.actual - altura.inicial) >= ALTURA_MAX) {
+      if (!tiempos.tiempo || motor.vel < 1300 || (diff) >= ALTURA_MAX) {
         ESC.writeMicroseconds(1000);
         digitalWrite(13, LOW);
         estado = PARADO;
       }
-      break;
-    case PARADO:
-      ESC.writeMicroseconds(1000);
-      leer_sensor();
       break;
   }
 }
@@ -95,7 +105,59 @@ void leer_sensor () {
   
   altura.actual = bmp280.readFloatAltitudeMeters();
 
-  if (altura.maxima < altura.actual) altura.maxima = altura.actual;
+  if (estado != ESPERA && altura.maxima < (altura.actual - altura.inicial)) altura.maxima = altura.actual - altura.inicial;
   
-  tiempos.sensor = 300;
+  tiempos.sensor = 1;
+}
+
+
+int reverse (int n) {
+  int reverse = 0, remainder;
+  while (n != 0) {
+    remainder = n % 10;
+    reverse = reverse * 10 + remainder;
+    n /= 10;
+  }
+  return reverse;
+}
+
+
+int alt_viendo = 0;
+int ult_digito;
+
+void actualizar_led_altura (int metros) {
+  if (tiempos.led_d) return;
+  
+  if (digitalRead(13)) {
+    digitalWrite(13, LOW);
+    tiempos.led_d = 1;
+    return;
+  }
+
+  // Reinicia el número
+  if (!alt_viendo && !ult_digito) {
+    if (metros % 10 == 0) metros++;
+    
+    alt_viendo = reverse(metros);
+    tiempos.led_d = 10;
+    return;
+  }
+
+  // Lee los dígitos
+  if (!ult_digito) {
+    ult_digito = alt_viendo % 10;
+    alt_viendo /= 10;
+    tiempos.led_d = 4;
+    if (!ult_digito) {
+      tiempos.led_d = 0;
+      delay(1000);
+      digitalWrite(13, HIGH);
+      delay(50);
+      digitalWrite(13, LOW);
+    }
+  } else {
+    tiempos.led_d = 1;
+    ult_digito--;
+    digitalWrite(13, HIGH);
+  }
 }
